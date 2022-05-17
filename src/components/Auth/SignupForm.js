c47419abc7c384e0827acda0c4f691cbcf3b3ac8
Eyfie <thomas.mendezdevigo@gmail.com>
2022-05-17 15:15:53 +0200

    Switch custom input for formik inputs

--- src/components/Auth/SignupForm.js
+++ src/components/Auth/SignupForm.js
@@ -1,54 +1,10 @@
 import React, { useState, useRef } from 'react'
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { signupSchema } from '../../validations/signupSchema';
-import CustomInput from '../Form/CustomInput';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faInfoCircle, faCheck } from '@fortawesome/free-solid-svg-icons';
 import axios from '../../api/axios';
 import { useNavigate } from 'react-router-dom';
-
-const inputs = [
-  {
-    id:1,
-    name: 'username',
-    type: 'text',
-    placeholder: 'Pseudo',
-    autoComplete: 'off',
-    required: true,
-  },
-  {
-    id:2,
-    name: 'firstname',
-    type: 'text',
-    placeholder: 'Prénom',
-    autoComplete: 'off',
-    required: true,
-  },
-  {
-    id:3,
-    name: 'lastname',
-    type: 'text',
-    placeholder: 'Nom',
-    autoComplete: 'off',
-    required: true,
-  },
-  {
-    id:4,
-    name: 'email',
-    type: 'email',
-    placeholder: 'Adresse e-mail',
-    autoComplete: 'off',
-    required: true,
-  },
-  {
-    id:5,
-    name: 'password',
-    type: 'password',
-    placeholder: 'Mot de passe',
-    autoComplete: 'off',
-    required: true,
-  },
-];
-
-
 
 
 const SignupForm = () => {
@@ -83,29 +39,134 @@ const SignupForm = () => {
   }
 
   return (
-    <section className='authentication border'>
-      <h2>Créez votre compte Groupomania</h2>
+    <section className='account'>
       <Formik 
         initialValues={{ username: '', firstname: '', lastname: '', email: '', password: '' }}
         validationSchema={ signupSchema }
         onSubmit={ handleSubmit }
       >
-       {(props) => (
-        <Form className='form signup-form'>
-          { inputs.map((input) => (<CustomInput key={ input.id } { ...input } />))}
-          <button className='focus-button' type='submit'>S'inscrire</button>
+
+       { ({ values, errors, touched }) => (
+        <Form className='form account-form'>
+          <div>
+            <h2>Informations du profil</h2>
+
+            <label htmlFor='username'>Nom d'utilisateur</label>
+            <div>
+              <Field 
+                type='text' 
+                name='username' 
+                id='username'
+                autoComplete='off'
+                placeholder='Votre pseudo'
+                aria-invalid={ errors.username ? 'false' : 'true' }
+                ariadescribedby='usernamenote'
+                />
+              { !touched.username || values.username === '' ?
+                null : errors.username && touched.username ?
+                  <span><FontAwesomeIcon icon={ faTimes }/></span>
+                  : <span><FontAwesomeIcon icon={ faCheck }/></span> }
+              { errors.username && touched.username ?
+                (<div id='usernamenote'><FontAwesomeIcon icon={ faInfoCircle }/>{ errors.username }</div>) 
+                : null }
+            </div>
+
+            <label htmlFor='firstname'>Prénom</label>
+            <div>
+              <Field 
+                type='text' 
+                name='firstname' 
+                id='firstname'
+                placeholder='Votre prénom'
+                aria-invalid={ errors.firstname ? 'false' : 'true' }
+                ariadescribedby='firstnamenote'
+                />
+              { !touched.firstname || values.firstname === '' ? 
+                null : errors.firstname && touched.firstname ?
+                  <span><FontAwesomeIcon icon={ faTimes }/></span>
+                  : <span><FontAwesomeIcon icon={ faCheck }/></span> }
+              { errors.firstname && touched.firstname ? 
+                (<div id='firstnamenote'><FontAwesomeIcon icon={ faInfoCircle } />{ errors.firstname }</div>) 
+                : null}
+              </div>
+            
+
+              <label htmlFor='lastname'>Nom</label>
+              <div>
+                <Field 
+                  type='text' 
+                  name='lastname' 
+                  id='lastname'
+                  placeholder='Votre nom'
+                  aria-invalid={ errors.lastname ? 'false' : 'true' }
+                  ariadescribedby='lastnamenote'
+                  />
+                { !touched.lastname || values.lastname === '' ?
+                  null : errors.lastname && touched.lastname ?
+                  <span><FontAwesomeIcon icon={ faTimes }/></span>
+                  : <span><FontAwesomeIcon icon={ faCheck }/></span> }
+                { errors.lastname && touched.lastname ? 
+                  (<div id='lastnamenote'><FontAwesomeIcon icon={ faInfoCircle } />{ errors.lastname }</div>) 
+                  : null}
+              </div>
+
+              <label htmlFor='email'>Adresse mail</label>
+              <div>
+                <Field 
+                  type='email' 
+                  name='email' 
+                  id='email'
+                  required
+                  placeholder='exemple@gmail.com'
+                  aria-invalid={ errors.email ? 'false' : 'true' }
+                  ariadescribedby='emailnote'
+                  />
+                { !touched.email || values.email === '' ?
+                  null : errors.email && touched.email ?
+                  <span><FontAwesomeIcon icon={ faTimes }/></span>
+                  : <span><FontAwesomeIcon icon={ faCheck }/></span> }
+                { errors.email && touched.email ? 
+                  (<div id='emailnote'><FontAwesomeIcon icon={ faInfoCircle } />{ errors.email }</div>) 
+                  : null}
+              </div>
+
+           
+
+            <label htmlFor='password'>Mot de passe actuel</label>
+            <div>
+              <Field 
+                type='password' 
+                name='password' 
+                id='password' 
+                placeholder='Votre mot de passe'
+                autoComplete='off'
+                required
+                aria-invalid={ errors.password ? 'false' : 'true' }
+                ariadescribedby='passwordnote'
+                />
+                { errors.newpassword && touched.newpassword ?
+                  <span><FontAwesomeIcon icon={ faTimes }/></span>
+                  : <span><FontAwesomeIcon icon={ faCheck }/></span> }
+                { errors.password && touched.password ? 
+                  (<div id='passwordnote'><FontAwesomeIcon icon={ faInfoCircle } />{ errors.password }</div>) 
+                  : null}
+            </div>
+          </div> 
+          <div>
+            <button className='auth-link__focus' type='submit'>S'inscrire</button>
+          </div>
         </Form>
        )}
       </Formik>
       <p 
-        ref={errorRef}
-        className={errorMsg ? 'error-msg' : 'offscreen'}
+        ref={ errorRef }
+        className={ errorMsg ? 'error-msg' : 'offscreen' }
         aria-live='assertive'
       >
-        {errorMsg}
+        { errorMsg }
       </p>
     </section>
-  );
+  )
 };
 
 export default SignupForm
