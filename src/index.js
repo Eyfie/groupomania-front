c9e0bfa71af32dd9bca375d19426a0fbdfecfb79
Eyfie <thomas.mendezdevigo@gmail.com>
2022-06-10 13:20:21 +0200

    Provider structure updated

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
 import App from './App';
+import './index.css';
 import { AuthProvider } from './contexts/AuthProvider';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if ( process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
+           <Routes>
+              <Route path='/*' element={<App />} />
+            </Routes>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
