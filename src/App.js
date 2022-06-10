36d2cb96e32925eb3b79be0cd2d9c54b3af35cf7
Eyfie <thomas.mendezdevigo@gmail.com>
2022-06-10 13:19:27 +0200

    Route structure updated

--- src/App.js
+++ src/App.js
@@ -14,16 +14,17 @@ import PostPage from "./pages/PostPage";
 import Unauthorized from "./pages/Unauthorized";
 
 //*Components
-import LoginForm from "./components/Form/Auth/LoginForm";
-import SignupForm from "./components/Form/Auth/SignupForm";
-import ForgotForm from "./components/Form/Auth/ForgotForm";
-import ForgotModifyForm from "./components/Form/Auth/ForgotModifyForm";
+import Login from "./components/Form/Auth/Login";
+import Signup from "./components/Form/Auth/Signup";
+import Forgot from "./components/Form/Auth/Forgot";
+import Modify from "./components/Form/Auth/Modify";
 import Account from "./components/Form/Account/Account";
 import Profil from "./components/Form/Account/Profil";
 
 
 
 function App() {
+
   return (
     <Routes>
       <Route path='/' element={ <Layout /> }>
@@ -31,30 +32,34 @@ function App() {
         {/* Public routes */}
 
         <Route  element={ <AuthPage /> }>
-          <Route path='login' element={ <LoginForm /> }/>
-          <Route path='signup' element={ <SignupForm /> }/>
-          <Route path='forgot' element={ <ForgotForm /> }/>
-          <Route path='modify' element={ <ForgotModifyForm /> }/>
+          <Route path='login' element={ <Login /> }/>
+          <Route path='signup' element={ <Signup /> }/>
+          <Route path='forgot' element={ <Forgot /> }/>
+          <Route path='modify' element={ <Modify /> }/>
           <Route path='unauthorized' element={ <Unauthorized /> }/>
         </Route>
 
         
-        <Route element={ <PersistLogin /> }>
-          
+        <Route element={ <PersistLogin /> }> 
+
           {/* Protected routes */}
           <Route element={ <RequireAuth allowedRoles={ ['user', 'moderator'] }/> }>
-            <Route path='/' element={ <HomePage /> }/>
-            <Route path='/'element={ <ProfilPage /> }>
-                <Route path='account' element={ <Account /> }/>
-                <Route path='profil' element={ <Profil /> }/>
-            </Route>
-            <Route path='post/:postid' element={ <PostPage /> }/>
-            <Route path='user/:userid' element={ <UserPage /> }/>
+
+              <Route path='/' element={ <HomePage /> }/>
+              <Route path='/'element={ <ProfilPage /> }>
+                  <Route path='account' element={ <Account /> }/>
+                  <Route path='profil' element={ <Profil /> }/>
+              </Route>
+
+              <Route path='post'>
+                <Route path=':postId' element={ <PostPage /> }/>
+              </Route>
+              <Route path='user'>
+                <Route path=':userId' element={ <UserPage /> }/>
+              </Route>
+
           </Route>
-
         </Route>
-
-
       </Route>
     </Routes>
   );
